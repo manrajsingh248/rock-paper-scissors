cfc10cd1a1169f19d44125fdabb5d2d689bdd928
manrajsingh248 <manraj.singh248@gmail.com>
2022-08-27 11:24:53 -0400

    Added playerScore and computerScore to result() function and added simple styling to the page

--- rps.js
+++ rps.js
@@ -2,16 +2,21 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+
 let player;
 let computer;
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
 
     player = button.textContent;
     computerSelection();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = result();
+    playerText.textContent = "Player: " + player;
+    computerText.textContent = "Computer: " + computer;
+    resultText.textContent = "Result: " + result();
 }));
 
 function computerSelection(){
@@ -30,17 +35,70 @@ function computerSelection(){
         break;
     }
 }
-function result(){
-    if(player == computer){
-      return "Draw!";
+
+
+function result() {
+
+    
+  
+
+
+    if ((player === "paper" && computer === "rock") || 
+        (player === "scissors" && computer === "paper") || 
+        (player === "rock" && computer === "scissors") && 
+        
+        (playerScore <=5 || computerScore <=5))
+
+        {
+       
+        playerScore += 1;
+        return ("You win! " + "Player score: " + playerScore + " Computer score: " + computerScore);
     }
-    else if ((player === "paper" && computer === "rock") ||
-            (player === "scissors" && computer === "paper") ||
-            (player === "rock" && computer === "scissors") ) {  
-        return ("You win! ");
+    
+    if (playerScore == 5) {
+        return ("Winner winner chicken dinner! You won the game! " + " Player score: " + playerScore + "Computer score: " + computerScore);
+        
     }
-    else {
-        return ("You lose :(");   
-}
-}
+    
+   
+    
+    else if 
+        ((player === "rock" && computer === "paper") || 
+        (player === "paper" && computer === "scissors") || 
+        (player === "scissors" && computer === "rock") &&
+         (playerScore <=5 || computerScore <=5))
+        
+        
+        {
+        
+        
+        computerScore += 1;
+        return("You lose! " + "Player score: " + playerScore + " Computer score: " + computerScore); 
+        }
+    
+    if (computerScore == 5) {
+        return ("You lost the game. " + "Player score: " + playerScore + " Computer score: " + computerScore);
+        }
+
+    else if
+    (player == computer) {
+        return ("Draw! " + "Player score: " + playerScore + " Computer score: " + computerScore);
+    } 
+
+
+}  
+      
+    
+
+    
+          
+    
+
+
+
+   
+   
+   
+   
+    
 
